d9d46219742763c406d8925e445febcaeaa89674
sopsop09 <sofiaarizka22@gmail.com>
2022-10-05 23:54:49 +0700

    Add files via upload

---
+++ study.php
@@ -0,0 +1,54 @@
+ <section id="resume" class="resume">
+     <div class="container">
+
+         <div class="section-title">
+             <h2>Resume</h2>
+         </div>
+
+         <div class="row">
+             <div class="col-lg-6" data-aos="fade-up">
+                 <h3 class="resume-title">Sumary</h3>
+                 <div class="resume-item pb-0">
+                     <h4>Sofia Arizka</h4>
+                     <p><em>I am a 5th semester student, I am currently attending the Merdeka Campus bootcamps at NF Computer.</em></p>
+                     <ul>
+                         <li>Tuasan street, Medan, Sumut, IND.</li>
+                         <li>0831-9820-1427</li>
+                         <li>sofi.sof@example.com</li>
+                     </ul>
+                 </div>
+
+                 <h3 class="resume-title">Education</h3>
+                 <div class="resume-item">
+                     <h4>MAN 1 MEDAN</h4>
+                     <h5>2017 - 2020</h5>
+                     <p><em>Organization</em></p>
+                     <ul>
+                         <li>Pramuka MAN 1 MEDAN</li>
+                     </ul>
+                 </div>
+                 <div class="resume-item">
+                     <h4>POLITEKNIK NEGERI MEDAN</h4>
+                     <h5>2020 - Now</h5>
+                     <p><em>Organization</em></p>
+                     <ul>
+                         <li>Badan EKsekutif Mahasiswa</li>
+                     </ul>
+                 </div>
+             </div>
+             <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+                 <h3 class="resume-title">Professional Experience</h3>
+                 <div class="resume-item">
+                     <h4>Staff Pengabdian Masyarakat</h4>
+                     <h5>2020</h5>
+                     <p><em>Politeknik Negeri Medan </em></p>
+                     <ul>
+                         <li>Ketua Panitia DEKAT 1.0</li>
+                         <li>Ketua Panitia DEKAT 2.0</li>                     
+                     </ul>
+                 </div>
+             </div>
+         </div>
+
+     </div>
+ </section>
